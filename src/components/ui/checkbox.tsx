d174832dd9ed1f9cc7fd2673c6f252264141522f
Artem Goldman <artygoldman@gmail.com>
2026-03-28 10:14:17 +0300

    Add missing label and checkbox UI components

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as React from 'react';
+
+export interface CheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  onCheckedChange?: (checked: boolean) => void;
+}
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className = '', onCheckedChange, onChange, ...props }, ref) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e);
+      onCheckedChange?.(e.target.checked);
+    };
+
+    return (
+      <input
+        type="checkbox"
+        ref={ref}
+        className={`h-4 w-4 shrink-0 rounded border border-gray-300 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+        onChange={handleChange}
+        {...props}
+      />
+    );
+  }
+);
+Checkbox.displayName = 'Checkbox';
+
+export { Checkbox };
